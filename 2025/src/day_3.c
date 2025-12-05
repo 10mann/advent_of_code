d9919223555b1aaf2faebd79f4cad6d6cb19fd34
Timann <timann.janitz@nxtech.no>
2025-12-05 19:17:42 +0100

    fix: Add solution for day 3 part 2

--- 2025/src/day_3.c
+++ 2025/src/day_3.c
@@ -41,6 +41,44 @@ static void update_state_1(char c, long* joltage)
     }
 }
 
+static void update_state_2(char c, long* joltage)
+{
+    static char battery[256] = {0};
+    static size_t index = 0U;
+
+    if((c >= '0') && (c <= '9'))
+    {
+        battery[index] = c;
+        index++;
+    }
+    else
+    {
+        char numbers[13] = {0};
+        size_t current_index = 0U;
+        long jolt = 0;
+        long power = 100000000000;
+
+        for(size_t num_index = 12U; num_index > 0U; num_index--)
+        {
+            for(size_t i = current_index; i <= (index - num_index); i++)
+            {
+                if(battery[i] > numbers[num_index - 1U])
+                {
+                    current_index = i + 1U;
+                    numbers[num_index - 1U] = battery[i];
+                }
+            }
+
+            jolt += (numbers[num_index - 1U] - '0') * power;
+            power /= 10;
+        }
+
+        (*joltage) += jolt;
+        memset(battery, 0, sizeof(battery));
+        index = 0U;
+    }
+}
+
 static void find_joltage_1(const char* filename)
 {
     long joltage = 0;
@@ -74,7 +112,52 @@ static void find_joltage_1(const char* filename)
     printf("%li\n", joltage);
 }
 
+static void find_joltage_2(const char* filename)
+{
+    long joltage = 0;
+    FILE* input_text = fopen(filename, "r");
+    while(input_text != NULL)
+    {
+        char buffer[8 * 1024] = {0};
+        size_t length = fread(buffer, sizeof(buffer[0]), sizeof(buffer), input_text);
+        if(length < sizeof(buffer))
+        {
+            buffer[length] = '\0';
+            length++;
+        }
+
+        for(size_t i = 0U; i < length; i++)
+        {
+            update_state_2(buffer[i], &joltage);
+        }
+
+        if(length < sizeof(buffer))
+        {
+            break;
+        }
+    }
+
+    if(input_text == NULL)
+    {
+        printf("Unable to open file %s\n", filename);
+    }
+
+    printf("%li\n", joltage);
+}
+
 void solve_day_3(const char* filename, int part)
 {
-    find_joltage_1(filename);
+    if(part == 1)
+    {
+        find_joltage_1(filename);
+    }
+    else if(part == 2)
+    {
+        find_joltage_2(filename);
+    }
+    else
+    {
+        find_joltage_1(filename);
+        find_joltage_2(filename);
+    }
 }
