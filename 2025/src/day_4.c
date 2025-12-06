c6d6b2537ae7b828546c066efc6b59d6792ffe41
Timann <timann.janitz@nxtech.no>
2025-12-06 19:13:49 +0100

    feat: Add solution for day 4 part 2

--- 2025/src/day_4.c
+++ 2025/src/day_4.c
@@ -36,7 +36,46 @@ static long find_accessible_first_or_last_1(const char* row_1, const char* row_2
                 num_accessible++;
             }
         }
+    }
 
+    return num_accessible;
+}
+
+static long find_accessible_first_or_last_2(const char* row_1, const char* row_2, size_t length, char* replace_row)
+{
+    long num_accessible = 0;
+    for(size_t i = 0U; i < length; i++)
+    {
+        replace_row[i] = row_1[i];
+        long count = 0;
+        if(row_1[i] == '@')
+        {
+            if((i > 0) && (row_1[i - 1] == '@'))
+            {
+                count++;
+            }
+            if((i < (length - 1U)) && (row_1[i + 1U] == '@'))
+            {
+                count++;
+            }
+            if(row_2[i] == '@')
+            {
+                count++;
+            }
+            if((i > 0) && (row_2[i - 1] == '@'))
+            {
+                count++;
+            }
+            if((i < (length - 1U)) && (row_2[i + 1U] == '@'))
+            {
+                count++;
+            }
+            if(count < 4)
+            {
+                num_accessible++;
+                replace_row[i] = '.';
+            }
+        }
     }
 
     return num_accessible;
@@ -92,6 +131,58 @@ static long find_accessible_1(const char* row_1, const char* row_2, const char* 
     return num_accessible;
 }
 
+static long find_accessible_2(const char* row_1, const char* row_2, const char* row_3, size_t length, char* replace_row)
+{
+    long num_accessible = 0;
+    for(size_t i = 0U; i < length; i++)
+    {
+        replace_row[i] = row_2[i];
+        long count = 0;
+        if(row_2[i] == '@')
+        {
+            if(row_1[i] == '@')
+            {
+                count++;
+            }
+            if((i > 0) && (row_1[i - 1] == '@'))
+            {
+                count++;
+            }
+            if((i < (length - 1U)) && (row_1[i + 1U] == '@'))
+            {
+                count++;
+            }
+            if((i > 0) && (row_2[i - 1] == '@'))
+            {
+                count++;
+            }
+            if((i < (length - 1U)) && (row_2[i + 1U] == '@'))
+            {
+                count++;
+            }
+            if(row_3[i] == '@')
+            {
+                count++;
+            }
+            if((i > 0) && (row_3[i - 1] == '@'))
+            {
+                count++;
+            }
+            if((i < (length - 1U)) && (row_3[i + 1U] == '@'))
+            {
+                count++;
+            }
+            if(count < 4)
+            {
+                num_accessible++;
+                replace_row[i] = '.';
+            }
+        }
+    }
+
+    return num_accessible;
+}
+
 static void update_state_1(char c, long* accessible_rolls)
 {
     static size_t index = 0U;
@@ -130,6 +221,43 @@ static void update_state_1(char c, long* accessible_rolls)
     }
 }
 
+static void update_state_2(char c, long* accessible_rolls)
+{
+    static size_t row = 0U;
+    static size_t column = 0U;
+    static char grid[256][256] = {0};
+    static char new_grid[256][256] = {0};
+
+    if((c == '\n') || (c == '\0'))
+    {
+        if(c == '\0')
+        {
+            long accessible = -1;
+            size_t index = 0U;
+            while((accessible != 0))
+            {
+                accessible = find_accessible_first_or_last_2(grid[0], grid[1], column, new_grid[0]);
+                for(size_t i = 1U; i < row; i++)
+                {
+                    accessible += find_accessible_2(grid[i - 1U], grid[i], grid[i + 1U], column, new_grid[i]);
+                }
+                accessible += find_accessible_first_or_last_2(grid[row], grid[row - 1], column, new_grid[row]);
+                (*accessible_rolls) += accessible;
+                memcpy(grid, new_grid, sizeof(grid));
+                index++;
+            }
+        }
+
+        row++;
+        column = 0U;
+    }
+    else
+    {
+        grid[row][column] = c;
+        column++;
+    }
+}
+
 static void find_accessible_rolls_1(const char* filename)
 {
     long accessible_rolls = 0;
@@ -159,7 +287,42 @@ static void find_accessible_rolls_1(const char* filename)
     {
         printf("Unable to open file %s\n", filename);
     }
+    
+    fclose(input_text);
+    printf("%li\n", accessible_rolls);
+}
 
+static void find_accessible_rolls_2(const char* filename)
+{
+    long accessible_rolls = 0;
+    FILE* input_text = fopen(filename, "r");
+    while(input_text != NULL)
+    {
+        char buffer[8 * 1024] = {0};
+        size_t length = fread(buffer, sizeof(buffer[0]), sizeof(buffer), input_text);
+        if(length < sizeof(buffer))
+        {
+            buffer[length] = '\0';
+            length++;
+        }
+
+        for(size_t i = 0U; i < length; i++)
+        {
+            update_state_2(buffer[i], &accessible_rolls);
+        }
+
+        if(length < sizeof(buffer))
+        {
+            break;
+        }
+    }
+
+    if(input_text == NULL)
+    {
+        printf("Unable to open file %s\n", filename);
+    }
+
+    fclose(input_text);
     printf("%li\n", accessible_rolls);
 }
 
@@ -171,10 +334,11 @@ void solve_day_4(const char* filename, int part)
     }
     else if(part == 2)
     {
-        
+        find_accessible_rolls_2(filename);
     }
     else
     {
-
+        find_accessible_rolls_1(filename);
+        find_accessible_rolls_2(filename);
     }
 }
